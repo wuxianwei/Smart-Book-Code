0a7f47c9a1d4bc023d718aa3851616abbbc8c029
MiracleWang <miracle.wang@rush.im>
2016-06-30 16:03:23 +0800

    Add the Test of the Code

--- chapter2/src/main/java/com/smart5j/chapter2/service/CustomerCreateService.java
+++ chapter2/src/main/java/com/smart5j/chapter2/service/CustomerCreateService.java
@@ -1,8 +1,8 @@
 package com.smart5j.chapter2.service;
 
 import com.smart5j.chapter2.model.Customer;
-import com.sun.tools.javac.comp.Todo;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -18,7 +18,16 @@ public class CustomerCreateService {
         // TODO: 16/6/29
         return  null;
     }
-
+    // 获取客户的列表
+    public List<Customer> getCustomerList() {
+        // TODO: 16/6/29
+        List<Customer> list = new ArrayList<Customer> ();
+        Customer customer1 = new Customer();
+        Customer customer2 = new Customer();
+        list.add(customer1);
+        list.add(customer2);
+        return  list;
+    }
     // 获取客户
     public Customer getCustomer(long id){
         // TODO: 16/6/29
@@ -39,6 +48,7 @@ public class CustomerCreateService {
     // 删除客户
     public boolean deleteCustomer(long id){
         // TODO: 16/6/29
-        return false;
+        // TODO: 16/6/30  
+        return true;
     }
 }
--- chapter2/src/test/java/com/smart5j/chapter2/test/CustomerServiceTest.java
+++ chapter2/src/test/java/com/smart5j/chapter2/test/CustomerServiceTest.java
@@ -1,7 +1,71 @@
 package com.smart5j.chapter2.test;
 
+import com.smart5j.chapter2.model.Customer;
+import com.smart5j.chapter2.service.CustomerCreateService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * Created by miraclewong on 16/6/29.
  */
 public class CustomerServiceTest {
+    
+    private  final CustomerCreateService customerCreateService;
+
+    public CustomerServiceTest() {
+        this.customerCreateService =new CustomerCreateService();
+    }
+    
+    @Before
+    public void init(){
+        // TODO: 16/6/30
+    }
+
+    @Test
+    public void getCustomerListTest() throws Exception{
+        List<Customer> customerList = customerCreateService.getCustomerList();
+        Assert.assertEquals(2,customerList.size());
+    }
+
+
+    @Test
+    public void getCustomerTest() throws  Exception{
+        long id = 1;
+        Customer customer = customerCreateService.getCustomer(id);
+        Assert.assertNotNull(customer);
+    }
+
+    @Test
+    public void createCustomerTest() throws  Exception{
+        Map<String, Object> fieldMap = new HashMap<String, Object>();
+        fieldMap.put("name", "customer001");
+        fieldMap.put("contact", "John");
+        fieldMap.put("telephone ", "15827198245");
+        fieldMap.put("name", "customer001");
+        boolean result = customerCreateService.createCustomer(fieldMap);
+
+        Assert.assertTrue(!result);
+    }
+
+    @Test
+    public void updateCustomerTest() throws Exception {
+        long id = 1;
+        Map<String, Object> fieldMap = new HashMap<String, Object>();
+        fieldMap.put("contact", "Eric");
+        boolean result = customerCreateService.updateCustomer(id, fieldMap);
+        Assert.assertTrue(!result);
+    }
+
+    @Test
+    public void deleteCustomerTest() throws Exception {
+        long id = 1;
+        boolean result = customerCreateService.deleteCustomer(id);
+        Assert.assertTrue(result);
+    }
 }
