f14be3e7bb923b8807f3fdd344cbe293294e1f1b
MiracleWang <miracle.wang@rush.im>
2016-06-30 20:07:58 +0800

    加入配置文件和工具类PropsUtil

---
+++ chapter2/src/main/java/com/smart5j/chapter2/util/PropsUtil.java
@@ -0,0 +1,32 @@
+package com.smart5j.chapter2.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by wangrui on 16/6/30.
+ *
+ * 属性文件的工具类
+ */
+public class PropsUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropsUtil.class);
+
+
+    /**
+     * 加载属性文件
+     */
+
+    public static Properties loadProps(String feleName){
+        Properties props = null;
+
+        InputStream inputStream = null;
+
+        try{
+            inputStream = Thread.currentThread()
+        }
+        return  props;
+    }
+}
